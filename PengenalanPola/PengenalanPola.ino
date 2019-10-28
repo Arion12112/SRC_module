542e5472e9a49f4878f5bad4110889981370e87d
Arion12112 <47907685+Arion12112@users.noreply.github.com>
2019-10-28 16:07:35 +0700

    pola x

--- PengenalanPola/PengenalanPola.ino
+++ PengenalanPola/PengenalanPola.ino
@@ -23,14 +23,20 @@ void loop()
     Serial.print("Input yang dimasukkan adalah: "); //mencetak string "Input yang dimasukkan adalah: " ke serial monitor
     Serial.println(readString); //mencetak nilai dari readString ke serial monitor
     x = readString.toInt(); //Memasukan isi dari variabel readString yang tipe datanya diubah menjadi integer ke variabel x 
-    for(i = 1; i <= x; i++)  //untuk nilai i = 1 lakukan perulangan sampai bernilai x dengan syarat setiap perulangan nilai i ditambah 1
-    { 
-      for(j = 1; j <= x; j++) //untuk nilai j = 1 lakukan perulangan sampai bernilai x dengan syarat setiap perulangan nilai j ditambah 1 
-      { 
-        if(j<=i) Serial.print("*"); //apabila nilai j kurang dari nilai i maka cetak * di serial monitor
-        else Serial.print(" "); //selainnya cetak "spasi" di serial monitor
-      }
-      Serial.println(); //mencetak newline(baris baru) di serial monitor 
+    for (i=1;i<=x;i++) //untuk nilai i = 1 lakukan perulangan sampai bernilai x dengan syarat setiap perulangan nilai i ditambah 1
+    {
+        for(j=1;j<=x;j++) //untuk nilai i = 1 lakukan perulangan sampai bernilai x dengan syarat setiap perulangan nilai i ditambah 1
+        {
+            if((i==j)||(i+j)%(x+1)==0) //jika nilai i sama dengan nilai j ATAU nilai i ditambah nilai j di modulo nilai x ditambah 1 sama dengan 0  
+            {
+            Serial.print("*"); //maka cetak *
+            }
+            else //selainnya
+            {
+            Serial.print(" "); //maka cetak spasi
+            }
+        }
+          Serial.println(); //apabila nilai j sama dengan nilai input x maka cetak baris baru 
     }
     readString=""; //mengosongkan isi dari variabel readString 
     Serial.println(); //mencetak newline(baris baru) di serial monitor     
